f9eeb11bb45927db0c6a5a01d86bd0ea39de0ec6
mertturk24 <49321825+mertturk24@users.noreply.github.com>
2019-04-13 15:23:08 +0300

    Add files via upload

---
+++ SayiTahmin/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hafta3
+{
+    public partial class Form1 : Form
+    {
+
+        int degisken2;
+        int degisken1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random salla = new Random();
+            degisken1 = salla.Next(1,100);
+           // label1.Text=degisken1.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            degisken2=Convert.ToInt32( textBox1.Text);
+
+            if (degisken1 == degisken2)
+            {
+                MessageBox.Show("Bildin");
+
+            }
+            else
+            {
+               
+                if (degisken1 > degisken2)
+                {
+
+                    MessageBox.Show("Yanlış Arttır");
+                }
+                else
+                {
+                    MessageBox.Show("Yanlış Azalt");
+                }
+            }
+
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
